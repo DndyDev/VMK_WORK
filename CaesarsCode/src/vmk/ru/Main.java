cba122847b896ada00b66058738d66a7d6cb1619
SITihomirov <sirtikhomirov@gmail.com>
2020-02-25 18:58:29 +0300

    add UF interface

--- CaesarsCode/src/vmk/ru/Main.java
+++ CaesarsCode/src/vmk/ru/Main.java
@@ -7,20 +7,25 @@ public class Main {
     public static void main(String[] args) {
         String alphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯaбвгдежзиклмнопрстуфхцчшщъыьэюя";
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите шаг шифрования");
         int step = scanner.nextInt();
-        String string = "Привет мир";
-//        int buffer;
-        for (int i = 0; i < string.length(); i++){
-             int buffer = alphabet.indexOf(string.charAt(i)); //
-             if(buffer == -1) {
-                 System.out.print(string.charAt(i));
-             } else{
-                 buffer =  (buffer + step) % alphabet.length();
-                 System.out.print(alphabet.charAt(buffer));
-             }
+        System.out.println("Шифруем текст или файл ?");
+        String answer = scanner.nextLine();
+        if(answer =="текст") {
+            String string = scanner.nextLine();
+            for (int i = 0; i < string.length(); i++){
+                int buffer = alphabet.indexOf(string.charAt(i)); //
+                if(buffer == -1) {
+                    System.out.print(string.charAt(i));
+                } else{
+                    buffer =  (buffer + step) % alphabet.length();
+                    System.out.print(alphabet.charAt(buffer));
+                }
+            }
         }
-
-
+        if(answer == "файл"){
+            
+        }
 
 
     }
