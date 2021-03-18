5fd4bc055e8f9e1ae2d3f7625e372f2625e0df0f
SITihomirov <sirtikhomirov@gmail.com>
2021-03-19 01:44:36 +0300

    update Servlets

---
+++ Servlet_Cookies_Session/src/LogoutServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+
+    }
+}
