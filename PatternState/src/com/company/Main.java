e93acd1542dc4c0c466fff744d2960bba2ec7139
SITihomirov <sirtikhomirov@gmail.com>
2021-03-18 22:54:42 +0300

    add all

---
+++ PatternState/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+import static com.company.State.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- Web_application/src/examples/HelloServlet.java
+++ Web_application/src/examples/HelloServlet.java
@@ -1,27 +1,34 @@
 package examples;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        String name = request.getParameter("username");
+        String age = request.getParameter("userage");
+        String gender = request.getParameter("gender");
+        String country = request.getParameter("country");
+        String[] courses = request.getParameterValues("courses");
 
-    public void destroy() {
+        try {
+            writer.println("<p>Name: " + name + "</p>");
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for(String course: courses)
+                writer.println("<li>" + course + "</li>");
+        } finally {
+            writer.close();
+        }
     }
 }
